03f240e916f720927b2046f7f161f47ce9c0d854
Luis Pimentel <luispimentel@Luiss-MBP.lan>
2023-05-02 19:40:40 -0400

    added main.css file

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,25 +1,11 @@
-// const router = require('express').Router()
-// const Bread = require('../models/bread')
 
-
-// router.get('/', (req, res) =>{
-//     res.render('index')
-// })
-
-// router.get('/:index', (req, res) =>{
-//     const { index } = res.params
-//     res.send(Bread[index])
-// })
-
-// module.exports = router
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('Index',
-      {
+    res.render('Index',{          
         breads: Bread,
         title: "Index Pages"
       }
@@ -29,7 +15,10 @@ breads.get('/', (req, res) => {
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+   const {arrayIndex} = req.params 
+  res.render('show', {
+      bread: Bread[arrayIndex]
+    })
   })
   
 
--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-// const express = require('express')
-// require('dotenv').config()
-// const breadController = require('./controllers/bread')
 
-// const app = express()
-
-// // MIDDLEWARE
-// app.set('views', __dirname + '/views')
-// app.set('view engine', 'jsx')
-// app.engine('jsx', require('express-react-views').createEngine())
-
-
-// app.use('/breads', breadController)
-
-// // const PORT = process.env.PORT
-
-// app.listen(8080)
 
 // DEPENDENCIES
 const express = require('express')
@@ -26,6 +10,7 @@ const breadsController = require('./controllers/bread.js')
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
